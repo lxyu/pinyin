373f408f48bea73c3a9bec41b24af8fad9e0c11b
lxyu <lixinfish@gmail.com>
2012-08-30 01:22:25 +0800

    make it a pypi package.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='pinyin',
+    version='0.1.0',
+    description='Translate chinese chars to pinyin based on Mandarin.dat',
+    author='Lx Yu',
+    author_email='lixinfish@gmail.com',
+    packages=['pinyin', ],
+    package_data={'': ['LICENSE'], },
+    url='https://github.com/lxyu/pinyin',
+    license=open('LICENSE').read(),
+    long_description=open('README.rst').read(),
+)
