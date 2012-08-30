d35f4e0064a9937ca211ea3bcf4c85e116fb0373
lxyu <lixinfish@gmail.com>
2012-08-30 23:22:44 +0800

    fix dat file not included bug

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 
 setup(
     name='pinyin',
-    version='0.1.0',
+    version='0.1.1',
     description='Translate chinese chars to pinyin based on Mandarin.dat',
     author='Lx Yu',
     author_email='lixinfish@gmail.com',
     packages=['pinyin', ],
-    package_data={'': ['LICENSE'], },
+    package_data={'': ['LICENSE'], 'pinyin': ['Mandarin.dat'], },
     url='https://github.com/lxyu/pinyin',
     license=open('LICENSE').read(),
     long_description=open('README.rst').read(),
