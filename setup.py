02bab5402c74d4d104cb1668043f2374bd04e77c
lxyu <lixinfish@gmail.com>
2012-09-04 14:07:39 +0800

    add a bin script

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ from distutils.core import setup
 
 setup(
     name='pinyin',
-    version='0.1.1',
+    version='0.1.2',
     description='Translate chinese chars to pinyin based on Mandarin.dat',
     author='Lx Yu',
     author_email='lixinfish@gmail.com',
     packages=['pinyin', ],
     package_data={'': ['LICENSE'], 'pinyin': ['Mandarin.dat'], },
-    url='http://lxyu.github.com/pinyin',
+    scripts=['bin/pinyin'],
+    url='http://lxyu.github.com/pinyin/',
     license=open('LICENSE').read(),
     long_description=open('README.rst').read(),
 )
