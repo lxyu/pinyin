610ff5c2df46a9aec30f53f68b5aff4472ed1c3c
Lx Yu <lixinfish@gmail.com>
2013-03-03 00:38:27 +0800

    add travis ci

--- test_pinyin.py
+++ test_pinyin.py
@@ -9,25 +9,25 @@ class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
     def test_get(self):
-        self.assertEqual(pinyin.get(u'你好'), u'nihao')
-        self.assertEqual(pinyin.get(u'你好吗?'), u'nihaoma?')
+        self.assertEqual(pinyin.get(u'你好'), 'nihao')
+        self.assertEqual(pinyin.get(u'你好吗?'), 'nihaoma?')
         self.assertEqual(pinyin.get(u'你好吗？'), u'nihaoma\uff1f')
 
         self.assertEqual(pinyin.get('你好'.decode('utf-8')), u'nihao')
 
     def test_get_initial(self):
         self.assertEqual(
-            pinyin.get_initial(u'你好'), u'n h')
+            pinyin.get_initial(u'你好'), 'n h')
         self.assertEqual(
-            pinyin.get_initial(u'你好吗?'), u'n h m ?')
+            pinyin.get_initial(u'你好吗?'), 'n h m ?')
         self.assertEqual(
             pinyin.get_initial(u'你好吗？'), u'n h m ？')
 
         self.assertEqual(
-            pinyin.get_initial('你好'.decode('utf-8')), u'n h')
+            pinyin.get_initial('你好'.decode('utf-8')), 'n h')
 
     def test_mixed_chinese_english_input(self):
-        self.assertEqual(pinyin.get(u'hi你好'), u'hinihao')
+        self.assertEqual(pinyin.get(u'hi你好'), 'hinihao')
 
     def test_unicode_assertion(self):
         self.failUnlessRaises(AssertionError, pinyin.get_pinyin, '你好')
