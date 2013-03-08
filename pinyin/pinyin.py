076dbf8d3aabefeba52a179cedcc822cc135c1f0
oldcai <albert.cai@auramarker.com>
2013-03-08 17:49:30 +0800

    forced return type to unicode

--- pinyin/pinyin.py
+++ pinyin/pinyin.py
@@ -26,41 +26,39 @@ def _pinyin_generator(chars):
         yield pinyin_dict.get(key, char), char
 
 
-def get(s, delimiter=''):
+def get(s, delimiter=u''):
     """Return pinyin of string, the string must be unicode
     """
     assert(isinstance(s, unicode))
-    result = []
+    words = []
     one_word = u""
     for word in _punct_re.split(s.lower()):
         for pinyin, char in _pinyin_generator(word):
             if pinyin != char:
                 if one_word:
-                    result.append(one_word)
+                    words.append(one_word)
                     one_word = u""
-                result.append(pinyin)
-                print 2
+                words.append(pinyin)
             else:
-                print 1
                 one_word += char
 
         if one_word:
-            result.append(one_word)
+            words.append(one_word)
             one_word = u""
 
-    return delimiter.join(result)
+    return unicode(delimiter.join(words))
 
 
 # This function is only for backward compatibility, use `get` instead.
 get_pinyin=get
 
 
-def get_initial(s, delimiter=' '):
+def get_initial(s, delimiter=u' '):
     """Return the 1st char of pinyin of string, the string must be unicode
     """
     assert(isinstance(s, unicode))
-    result = []
+    words = []
     for word in _punct_re.split(s.lower()):
         for pinyin, char in _pinyin_generator(word):
-            result.append(pinyin[0])
-    return delimiter.join(result)
+            words.append(pinyin[0])
+    return unicode(delimiter.join(words))
