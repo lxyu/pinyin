68b848138d1bb1b93e2c57b08a2d19f02d5db2d3
Lx Yu <lixinfish@gmail.com>
2014-02-25 15:42:16 +0800

    it's 2014 now.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='pinyin',
-    version='0.2.3',
+    version='0.2.4',
     description='Translate chinese chars to pinyin based on Mandarin.dat',
     author='Lx Yu',
-    author_email='lixinfish@gmail.com',
+    author_email='github@lxyu.net',
     packages=['pinyin', ],
     package_data={'': ['LICENSE'], 'pinyin': ['Mandarin.dat'], },
     entry_points={"console_scripts": ["pinyin = pinyin.cmd:pinyin", ]},
-    url='http://lxyu.github.com/pinyin/',
+    url='http://lxyu.github.io/pinyin/',
     license=open('LICENSE').read(),
     long_description=open('README.rst').read(),
 )
